eabcafdce11df070e598f90bb1c5f14e68c0f2af
Kate Martin <kate@renanthera.lgbt>
2022-10-30 02:46:41 -0600

    added some comments

--- python/main.py
+++ python/main.py
@@ -1,8 +1,14 @@
 from wcl import *
 from caching import *
 
-
 # wcl.py conatins some magic values for access to v2 api and local filenames of client secrets
+
+# MANUAL
+# enter warcraftlogs reportCode string as reportCode
+# filter encounter selection gui with integer array of encounter ids
+# construct basic queries that support caching.
+# for more complicated queries, caching may not work. utilize the returnQuery method within wcl.py.
+# process demonstrated in cachedReturnQuery of caching.py
 
 reportCode = 'RL1v2DbxckJ9dTWq'
 encounterIDBlacklist = []
@@ -15,31 +21,28 @@ dataType = 'Healing'
 fields = ['data','reportData','report','events','data']
 absorbTicks = cachedReturnQuery(reportCode, startTime, endTime, id, abilityID, dataType, fields)
 
-# eventSlice = completeEvent(startTime, endTime, id, abilityID, dataType)
-# absorbTicks = returnQuery(reportCode, eventSlice, fields)
+# purification casts
+abilityID = '119582'
+dataType = 'Casts'
+fields = ['data','reportData','report','events','data']
+absorbTicks = cachedReturnQuery(reportCode, startTime, endTime, id, abilityID, dataType, fields)
 
-# dumpArtifact(absorbTicks, 'absorbTicks')
+# stagger damage tick events
+abilityID = '124255'
+dataType = 'DamageTaken'
+fields = ['data','reportData','report','events','data']
+absorbTicks = cachedReturnQuery(reportCode, startTime, endTime, id, abilityID, dataType, fields)
 
-# # purification casts
-# abilityID = '119582'
-# dataType = 'Casts'
-# fields = ['data','reportData','report','events','data']
-# eventSlice = completeEvent(startTime, endTime, id, abilityID, dataType)
-# purificationCasts = returnQuery(reportCode, eventSlice, fields)
+# brewmaster's rhythm
+abilityID = '394797'
+dataType = 'Buffs'
+fields = ['data','reportData','report','events','data']
+absorbTicks = cachedReturnQuery(reportCode, startTime, endTime, id, abilityID, dataType, fields)
 
-# # stagger damage tick events
-# abilityID = '124255'
-# dataType = 'DamageTaken'
-# fields = ['data','reportData','report','events','data']
-# eventSlice = completeEvent(startTime, endTime, id, abilityID, dataType)
-# damageTicks = returnQuery(reportCode, eventSlice, fields)
+# print api points spent to console (so you can see if you're an idiot)
+pointsSpent()
 
-# # brewmaster's rhythm
-# abilityID = '394797'
-# dataType = 'Buffs'
-# fields = ['data','reportData','report','events','data']
-# eventSlice = completeEvent(startTime, endTime, id, abilityID, dataType)
-# brewmasterRhythm = returnQuery(reportCode, eventSlice, fields)
+# this will get removed once the c++ implementation is completed. exists as notes until then.
 
 # s = []
 # print(absorbTicks[0])
@@ -63,5 +66,3 @@ absorbTicks = cachedReturnQuery(reportCode, startTime, endTime, id, abilityID, d
 # bnw = staggerFabrication(s, p=1)
 # opt = {'ticks':20,'purification':0.5,'t29':True,'dcheck':False}
 # base = staggerFabrication(s, opt, p=1)
-
-pointsSpent()
