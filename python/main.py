76b09ca68db17efa7a505d9029262f39ef9bf39a
Kate Martin <51387586+renanthera@users.noreply.github.com>
2023-11-25 17:01:33 -0700

    OH NO WHY DID I EVEN

--- python/main.py
+++ python/main.py
@@ -6,6 +6,7 @@ import time
 import Stagger
 
 from itertools import chain
+from collections import defaultdict
 
 def fmt(v):
     vals = 'kmbt'
@@ -33,9 +34,9 @@ wcl.pointsSpent()
 
 # report code
 reportCode = '7xNnGhDMK19vtVYF'
+reportCode = '48WF9r1QMZLaCdxm'
 
 # gather segments to input start and end time
-p(wcl_n.getSegments(reportCode))
 
 # Valdrakken Dummies (CASE 1)
 startTime = 34
@@ -49,13 +50,92 @@ endTime = 1573769
 startTime = 10116755
 endTime = 10416126
 
+reports = {
+    'affinitym-11/25-a' : {
+        'reportCode': '48WF9r1QMZLaCdxm',
+        'startTime': 4028035,
+        'endTime': 5649260,
+        'id': '6',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis',
+        'complete': True
+    },
+    'affinitym-11/25-b' : {
+        'reportCode': '48WF9r1QMZLaCdxm',
+        'startTime': 1585659,
+        'endTime': 3304850,
+        'id': '6',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis'
+    },
+    'relowindi-11/23-a' : {
+        'reportCode': '1kZbDcdynvL9YQqm',
+        'startTime': 6885007,
+        'endTime': 7928641,
+        'id': '366',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis',
+        'complete': True
+    },
+    'relowindi-11/23-b' : {
+        'reportCode': '1kZbDcdynvL9YQqm',
+        'startTime': 8100157,
+        'endTime': 9162810,
+        'id': '366',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis'
+    },
+    'relowindi-11/23-b' : {
+        'reportCode': '1kZbDcdynvL9YQqm',
+        'startTime': 8100157,
+        'endTime': 9162810,
+        'id': '366',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis'
+    },
+    'relowindi-11/23-c' : {
+        'reportCode': '1kZbDcdynvL9YQqm',
+        'startTime': 2967174,
+        'endTime': 4250734,
+        'id': '366',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis'
+    },
+    'relowindi-11/23-d' : {
+        'reportCode': '1kZbDcdynvL9YQqm',
+        'startTime': 116652,
+        'endTime': 1815969,
+        'id': '366',
+        'added': '11/25',
+        'reason': 't31 4p proc analysis'
+    },
+    # 'captclit-11/21-a'
+}
 
+def isComplete():
+    complete = {}
+    for k,v in reports.items():
+        reportCode = v.get('reportCode')
+        if not complete.get(reportCode):
+            complete.update({reportCode: v.get('complete', False)})
+        else:
+            if complete.get(reportCode) and v.get('complete', False):
+                complete.update({reportCode: True})
+    return complete
+
+def getSegments():
+    complete = isComplete()
+    for k, v in complete.items():
+        if not v:
+            p(wcl_n.getSegments(k))
+
+getSegments()
 # gather player info to enter player id
 # p(wcl_n.getPlayerInfo(reportCode, startTime, endTime))
 # p(wcl_n.getMasterData(reportCode))
 
 # sometimes neither work, and you just have to get it from the UI?
-id = str(20)
+# id = str(6)
 
 # assert(False)
 
@@ -65,57 +145,138 @@ id = str(20)
 # casts        = wcl_n.getEvents(reportCode, None, {'dataType':'Casts',       'sourceID':id, 'translate':'true', 'startTime':startTime, 'endTime':endTime,     'useAbilityIDs':'true'})
 # damage_taken = wcl_n.getEvents(reportCode, None, {'dataType':'DamageTaken', 'sourceID':id, 'translate':'true', 'startTime':startTime, 'endTime':endTime,     'useAbilityIDs':'true'})
 
-charred_dreams                 = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'abilityID': 425299, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
-shadowflame_wreathe            = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'abilityID': 406764, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
-# elementium_pocket_anvil      = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'abilityID': 425299, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
-celestial_brew                 = wcl_n.getEvents(reportCode, None, {'dataType': 'Buffs', 'abilityID':      425965, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
-charred_dreams_data            = list(chain.from_iterable(charred_dreams.data))
-shadowflame_wreathe_data       = list(chain.from_iterable(shadowflame_wreathe.data))
-# elementium_pocket_anvil_data = list(chain.from_iterable(elementium_pocket_anvil.data))
-celestial_brew_data            = list(chain.from_iterable(celestial_brew.data))
+def tier_guard_analysis():
+    charred_dreams                 = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'abilityID': 425299, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
+    shadowflame_wreathe            = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'abilityID': 406764, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
+    celestial_brew                 = wcl_n.getEvents(reportCode, None, {'dataType': 'Buffs', 'abilityID':      425965, 'startTime': startTime, 'endTime': endTime, 'useAbilityIDs': 'true', 'sourceID': id})
+    charred_dreams_data            = list(chain.from_iterable(charred_dreams.data))
+    shadowflame_wreathe_data       = list(chain.from_iterable(shadowflame_wreathe.data))
+    celestial_brew_data            = list(chain.from_iterable(celestial_brew.data))
 
-# flatten data
-# healing_data      = list(chain.from_iterable(healing.data))
-# buffs_data        = list(chain.from_iterable(buffs.data))
-# casts_data        = list(chain.from_iterable(casts.data))
-# damage_taken_data = list(chain.from_iterable(damage_taken.data))
+    # flatten data
+    # healing_data      = list(chain.from_iterable(healing.data))
+    # buffs_data        = list(chain.from_iterable(buffs.data))
+    # casts_data        = list(chain.from_iterable(casts.data))
+    # damage_taken_data = list(chain.from_iterable(damage_taken.data))
 
-# merge data and sort by timestamp
-data = sorted(
-    # healing_data + buffs_data + casts_data + damage_taken_data,
-    # healing_data,
-    charred_dreams_data + celestial_brew_data + shadowflame_wreathe_data, # + elementium_pocket_anvil_data,
-    key=lambda s: (s.get('timestamp')))
+    # merge data and sort by timestamp
+    data = sorted(
+        # healing_data + buffs_data + casts_data + damage_taken_data,
+        # healing_data,
+        charred_dreams_data + celestial_brew_data + shadowflame_wreathe_data, # + elementium_pocket_anvil_data,
+        key=lambda s: (s.get('timestamp')))
 
-acc = 0
-amt = 0
-ct = 0
-for k in data:
-    spell_id = k.get('abilityGameID')
-    if spell_id == 425299:
-        amt += k.get('unmitigatedAmount')
-        # print(k.get('sourceID'), k.get('targetID'))
-        # amt += k.get('amount')
-    # if spell_id == 425965 and k.get('type') == 'removebuff':
-    #     print(k)
-    #     amt = 0
-    if spell_id == 425965 and k.get('type') == 'applybuff':
-        absorb = k.get('absorb')
-        reverse_crit_vers = absorb / (1 + 0.8 * 0.4007) / (1 + 0.2165)
-        acc += absorb
-        ct += 1
-        print('Absorb:', k.get('absorb'))
-        print('Damage Dealt since last:', amt)
-        print('Difference:', amt - absorb)
-        # print('Reverse crit and vers:', reverse_crit_vers)
-        if amt > 0:
-            print('Ratio:', absorb / amt)
-            print('Inverse ratio:', amt / absorb)
-        # print('Reverse Ratio:', reverse_crit_vers / amt)
-        print()
-        amt = 0
+    acc = 0
+    amt = 0
+    ct = 0
+    for k in data:
+        spell_id = k.get('abilityGameID')
+        if spell_id == 425299:
+            amt += k.get('unmitigatedAmount')
+            # print(k.get('sourceID'), k.get('targetID'))
+            # amt += k.get('amount')
+        # if spell_id == 425965 and k.get('type') == 'removebuff':
+        #     print(k)
+        #     amt = 0
+        if spell_id == 425965 and k.get('type') == 'applybuff':
+            absorb = k.get('absorb')
+            reverse_crit_vers = absorb / (1 + 0.8 * 0.4007) / (1 + 0.2165)
+            acc += absorb
+            ct += 1
+            print('Absorb:', k.get('absorb'))
+            print('Damage Dealt since last:', amt)
+            print('Difference:', amt - absorb)
+            # print('Reverse crit and vers:', reverse_crit_vers)
+            if amt > 0:
+                print('Ratio:', absorb / amt)
+                print('Inverse ratio:', amt / absorb)
+            # print('Reverse Ratio:', reverse_crit_vers / amt)
+            print()
+            amt = 0
 
-print(acc / ct)
+    print(acc / ct)
+
+def tier_proc_analysis():
+    hits       = wcl_n.getEvents(reportCode, None, {'dataType': 'DamageDone', 'startTime': startTime, 'endTime': endTime,     'useAbilityIDs': 'true', 'sourceID': id})
+    bof_debuff = wcl_n.getEvents(reportCode, None, {'dataType': 'Debuffs',    'abilityID': 123725,    'startTime': startTime, 'endTime': endTime,      'useAbilityIDs': 'true', 'sourceID': id})
+
+    hits_data        = list(chain.from_iterable(hits.data))
+    bof_debuff_data = list(chain.from_iterable(bof_debuff.data))
+
+    data = sorted(
+        hits_data + bof_debuff_data,
+        key=lambda s: (s.get('timestamp'))
+    )
+
+    debuff_active = defaultdict(int)
+    total = defaultdict(int)
+    active_targets = {}
+    for event in data:
+        ident = str(event.get('targetID', 0)) + '-' + str(event.get('targetInstance', 0))
+        spellid = event.get('abilityGameID', -1)
+        match event.get('type'):
+            case 'applydebuff':
+                # print('ID', event.get('targetID'))
+                # print('Instance', event.get('targetInstance'))
+                active_targets.update({ ident : True })
+            case 'removedebuff':
+                active_targets.update({ ident : False })
+            case 'refreshdebuff':
+                continue
+            case 'damage':
+                # print(event)
+                total[spellid] += 1
+                if active_targets.get(ident):
+                    debuff_active[spellid] += 1
+                continue
+            case _:
+                print('FELL THROUGH')
+                print(event)
+    # for k,v in active_targets.items():
+    #     print(k, v)
+    for k,v in debuff_active.items():
+        print(k, v, total[k])
+    def sum_hits(whitelist, match_total):
+        sum = 0
+        for spellid, count in total.items():
+            if spellid in whitelist:
+                if match_total:
+                    sum += total.get(spellid, 0)
+                else:
+                    sum += debuff_active.get(spellid, 0)
+        return sum
+
+    whitelist_trigger_4p = [
+        115181, # breath of fire hit
+        387621, # dragonfire brew hit
+        205523, # blackout kick hit
+        325153, # exploding keg hit
+        388867, # exploding keg proc
+        391400, # resonant fists hit
+        107270, # spinning crane kick hit
+        107428, # rising sun kick hit
+        148187, # rushing jade wind hit
+        389541, # claw of the white tiger hit
+        322109, # touch of death hit
+        1,      # melee hit
+        115129, # expel harm hit
+        227291, # stomp hit
+        132467, # chi wave hit
+        100780, # tiger palm hit
+    ]
+    whitelist_trigger_2p = [
+        115181, # breath of fire hit
+        123725, # breath of fire dot
+    ]
+    triggers_total = sum_hits([425299], True)
+    triggers_2p = sum_hits(whitelist_trigger_2p, True)
+    triggers_4p = triggers_total - triggers_2p
+    candidates_4p = sum_hits(whitelist_trigger_4p, False)
+    ratio = triggers_4p / candidates_4p
+    print(triggers_total, triggers_2p, triggers_4p, candidates_4p)
+    print(ratio)
+
+# tier_proc_analysis()
 
 # total = 0
 # ids_map = {}
